fbe032009f1f63fe2e214042d3aacbe8bf8b8c30
wojiaochadi <264835229@qq.com>
2020-01-08 18:43:11 +0800

    @RequestMapping修饰类、方法 //RequestMapping 路径定义  web项目/类/方法   所以类没有定义的话就是： web项目/方法 RequestMapping 映射 请求参数、请求方法、请求头
    Ant 风格资源地址支持 3 种匹配符：
         ?：匹配文件名中的一个字符
         *：匹配文件名中的任意字符
         **：** 匹配多层路径
    
    @PathVariable 可以来映射 URL 中的占位符到目标方法的参数中
    
    Rest 风格的 URL.
    需要配置过滤器：
        <!-- 配置 org.springframework.web.filter.HiddenHttpMethodFilter: 可以把 POST 请求转为 DELETE 或 POST 请求 -->
        <filter>
            <filter-name>HiddenHttpMethodFilter</filter-name>
            <filter-class>org.springframework.web.filter.HiddenHttpMethodFilter</filter-class>
        </filter>
        <filter-mapping>
            <filter-name>HiddenHttpMethodFilter</filter-name>
            <url-pattern>/*</url-pattern>
        </filter-mapping>

--- SpringMVC_1/src/main/WebContent/index.jsp
+++ SpringMVC_1/src/main/WebContent/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" isErrorPage="true" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -23,6 +23,23 @@
 	<a href="springmvc/testAntPath/ASDSFWEAREWRFEWTDSAGDFGREYTR/abc">testAntPath</a>
 	<br>
 	<a href="springmvc/testPathVariable/102">testPathVariable</a>
-
+	<br>
+	==========REST===========
+	<br>
+	<a href="springmvc/testRest/1">Test Rest Get</a>
+	<br>
+	<form action="/springmvc/testRest" method="post">
+		<input type="submit" value="TestRest POST">
+	</form>
+	<br>
+	<form action="/springmvc/testRest/1" method="post">
+		<input type="hidden" name="_method" value="PUT">
+		<input type="submit" value="TestRest PUT">
+	</form>
+	<br>
+	<form action="/springmvc/testRest/1" method="post">
+		<input type="hidden" name="_method" value="DELETE">
+		<input type="submit" value="TestRest DELETE">
+	</form>
 </body>
 </html>
--- SpringMVC_1/src/main/java/com/atguigu/springmvc/handlers/SpringMVCTest.java
+++ SpringMVC_1/src/main/java/com/atguigu/springmvc/handlers/SpringMVCTest.java
@@ -70,4 +70,45 @@ public class SpringMVCTest {
         System.out.println("testPathVariable: " + id);
         return SUCCESS;
     }
+
+    /**
+     * Rest 风格的 URL.
+     * 以 CRUD 为例:
+     * 新增: /order POST
+     * 修改: /order/1 PUT update?id=1
+     * 获取:/order/1 GET get?id=1
+     * 删除: /order/1 DELETE delete?id=1
+     *
+     * 如何发送 PUT 请求和 DELETE 请求呢 ?
+     * 1. 需要配置 HiddenHttpMethodFilter
+     * 2. 需要发送 POST 请求
+     * 3. 需要在发送 POST 请求时携带一个 name="_method" 的隐藏域, 值为 DELETE 或 PUT
+     *
+     * 在 SpringMVC 的目标方法中如何得到 id 呢? 使用 @PathVariable 注解
+     *
+     */
+    @RequestMapping(value = "/testRest/{id}", method = RequestMethod.GET)
+    public String testRest(@PathVariable Integer id) {
+        System.out.println("testRest GET: " + id);
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testRest", method = RequestMethod.POST)
+    public String testRest() {
+        System.out.println("testRest POST");
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testRest/{id}", method = RequestMethod.PUT)
+    public String testRestPut(@PathVariable Integer id) {
+        System.out.println("testRest PUT: " + id);
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testRest/{id}", method = RequestMethod.DELETE)
+    public String testRestDelete(@PathVariable Integer id) {
+        System.out.println("testRest DELETE: " + id);
+        return SUCCESS;
+    }
+
 }
